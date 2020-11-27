6475fa71a35cb2630e8a6c34b8c860399cefe9e1
VIKASH KAUSHIK <58074696+vikash232@users.noreply.github.com>
2020-11-27 11:53:02 +0530

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,38 @@
+import os
+print("________________________________________________")
+print("\n..........WELCOME.........")
+print("________________________________________________")
+
+print("\n\n.........SELECT YOUR INTREST .........")
+while True:
+		print("""\n 1.Hadoop
+				 \n 2.AWS 
+				 \n 3.Docker
+				 \n 4.Linux and Networking
+				 \n 5.LVM with hadoop
+				 \n 6.View Team Members
+				 \n 7.exit""")
+		print("\n Press the No. According to your Intrest!!")
+		choise = input("Enter your Choice: ")
+		if(choise == "1"):
+			print('hello')#hadoop file
+		elif(choise == "2"):
+			os.system('python3 Autoaws.py')#AWS
+		elif(choise == "3"):
+			os.system('python3 docker.py')#Docker
+		elif(choise == "4"):
+			os.system('python3 Linux-networking.py')#LinuxandNetworking
+		elif(choise == "5"):
+			os.system('python3 hadoop_with_lvm.py')#lvm
+		elif(choise == "6"):
+			print("============")
+			print("Team Members")
+			print("============")
+			print("""\n -> Vikash Kaushik
+					\n ->Monika Poddar
+					\n ->Parag Rahate
+					\n ->Pooja Maurya""")
+		elif(choise == "7"):
+			exit()			
+
+menu() 
